72aa832c22cfef4a9a20b66e4b07d618a9ed52c6
Aart Odding <aart_odding@hotmail.com>
2017-01-18 16:26:46 +0100

    player

---
+++ src/game/player/Player.java
@@ -0,0 +1,8 @@
+package game.player;
+
+import game.player.strategy.Strategy;
+
+public interface Player {
+	
+	
+}
---
+++ src/game/player/strategy/Strategy.java
@@ -0,0 +1,6 @@
+package game.player.strategy;
+
+
+public interface Strategy {
+
+}
