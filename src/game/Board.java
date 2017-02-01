aaa3a08cd1a9dae62443eb18eef0363d556c0810
Richard Kok <r.c.kok@student.utwente.nl>
2017-02-01 21:36:02 +0100

    Javadoc/JML

--- src/game/Board.java
+++ src/game/Board.java
@@ -4,27 +4,51 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import util.exception.ColumnFullException;
 
+/**
+ * @author Aart Odding.
+ * Board class representing a 3D connect4 board.
+ * A board consists of WIDTH * DEPTH columns, each can be filled with HEIGHT marks.
+ * When a block has been cascased vertically down the column, which normally happens due to gravity,
+ * the board looks in all possible directions to see if there are enough consecutive marks to win.
+ */
 public class Board {
 	
-	public static final int WIDTH = 4;
-	public static final int DEPTH = 4;
+	public static final int WIDTH = 4; 
+	public static final int DEPTH = 4; 
 	public static final int HEIGHT = 4;
-	public static final int N_FIELDS = WIDTH * DEPTH * HEIGHT;
-	public static final String DELIMETER = "+---+---+---+---+";
+	public static final int N_FIELDS = WIDTH * DEPTH * HEIGHT; //Possible positions to fill a mark.
 	
 	
-	public Mark[] fields;
-	private Game.Ending ending;
+	public Mark[] fields;       // all possible fields in width, depth and height of the board.
+						 	    //invariant: fields.length == N_FIELDS
+	private Game.Ending ending; //Indicates the current state of the game.
+							    //invariant: ending != null
 	
 	
-	// default constructor
+	/**
+	 * Creates a board, fills it with empty fields and gives it an ending state of not ended.
+	 */
+	/* @ensures fields.length = N_FIELDS
+	 * 			(\forall int i; 0 <= i && i < fields.length;
+ 					fields[i] == Mark.EMPTY); 
+ 				ending == Ending.NOT_ENDED */
+		
 	public Board() {
 		fields = new Mark[N_FIELDS];
 		Arrays.fill(fields, Mark.EMPTY);
 		ending = Game.Ending.NOT_ENDED;
 	}
 	
-	// copy constructor (more efficient than deepCopy function)
+	
+	/**
+	 * Constructor that copies a board.
+	 * @param board. The board to be copied.
+	 */
+	/* @requires board != null
+	 * @ensures fields.length = N_FIELDS
+	 * 			(\forall int i; 0 <= i && i < fields.length;
+ 	 *				fields[i] == Mark.EMPTY); 
+     *				ending == Ending.NOT_ENDED */
 	public Board(Board board) {
 		fields = new Mark[N_FIELDS];
 		for (int i = 0; i < N_FIELDS; ++i) {
@@ -34,29 +58,50 @@ public class Board {
 	}
 	
 	
+	///////////////////////////////////////////////////////////////
+	//                                                           //
+	//    					Queries 						     //
+	//                                                           //
+	///////////////////////////////////////////////////////////////
+	
+	
+	/**
+	 * Returns the height of the given column.
+	 * @param column. The column for which the height is checked. 
+	 * @return height of the column.
+	 */
+	/* @requires column != null;
+	 * @ensures\result >= 0 & /result < HEIGHT. */
 	public int getColumnHeight(Column column) {
 		return cascade(column).z;
 	}
 
 	
-	// pass by value to avoid messing with board from outside of class
+	/**
+	 * Returns the current state of the game.
+	 * @return a copy of the ending state.
+	 */
+	 /* @ensures \result != null;  */
 	public Game.Ending getEnding() {
 		return ending.copy();
 	}
 	
-	
+	/**
+	 * Checks wether the column is full.
+	 * @param column 	the column to be checked.
+	 * @return if the column has all positions not empty.
+	 */
+	/* @require column != null */
 	public boolean isColumnFull(Column column) {
 		return fields[Position.indexOf(column.x, column.y, HEIGHT - 1)] != Mark.EMPTY;
 	}
 	
 	
-	public void doMove(Move move) {
-		Position position = cascade(move.column);
-		fields[position.index()] = move.mark;
-		ending = checkEndingFor(position, move.mark);
-	}
-	
-	
+	/**
+	 * Returns an arraylist of all the columns that are not full.
+	 * @return arraylist of non-empty columns.
+	 */
+	/* @ensures\result >= 0 & \result < WIDTH * DEPTH */
 	public ArrayList<Column> possibleColumns() {
 		ArrayList<Column> possible = new ArrayList<Column>();
 		for (int x = 0; x < WIDTH; ++x) {
@@ -71,22 +116,43 @@ public class Board {
 	}
 	
 	
-	public String getRowAsString(int y, int z) {
-		String row = "|";
-		Direction direction = new Direction(
-				Direction.Axis.POSITIVE, Direction.Axis.NEUTRAL, Direction.Axis.NEUTRAL);
-		for (Position p = new Position(0, y, z); p != null; p = p.inDirection(direction)) {
-			row += " " + fields[p.index()].toString() + " |";
-		}
-		return row;
+	///////////////////////////////////////////////////////////////
+	//                                                           //
+	//    					Public Commands		     		     //
+	//                                                           //
+	///////////////////////////////////////////////////////////////
+	
+	
+	/**
+	 * Cascades down the column of the move parameter
+	 * and fills the lowest empty field with the mark of the move.
+	 * @param move 	the move with a column and a mark.
+	 */
+	/* @ensures notEmptyFields.length >= old.otEmptyFields.length
+	 */
+	public void doMove(Move move) {
+		Position position = cascade(move.column);
+		fields[position.index()] = move.mark;
+		ending = checkEndingFor(position, move.mark);
 	}
 	
 	
-	//==============================================//
-	//             PRIVATE FUNCTIONS                //
-	//==============================================//
+	///////////////////////////////////////////////////////////////
+	//                                                           //
+	//    				Private Commands					     //
+	//                                                           //
+	///////////////////////////////////////////////////////////////
 	
-	
+    /**
+     * Returns the ending state of the game by checking 
+     * for consecutive marks from the given position.
+     * Should be called after every new move since not all previous positions are checked. 
+     * @param position with a x, y and a z.
+     * @param mark	   either X or O.
+     * @return ending state of the game from a given position and mark.
+     */
+	/* @ensures \result == DRAW ||  \result == X_WINS ||
+	 * 		    \result == O_WINS ||  \result == NOT_ENDED */
 	private Game.Ending checkEndingFor(Position position, Mark mark) {
 		if (isFull()) {
 			return Game.Ending.DRAW;
@@ -106,7 +172,10 @@ public class Board {
 		return Game.Ending.NOT_ENDED;
 	}
 	
-	
+	/**
+	 * Returns wether the board is full.
+	 * @return true if board is full, false if board is not full
+	 */
 	private boolean isFull() {
 		for (int x = 0; x < WIDTH; ++x) {
 			for (int y = 0; y < DEPTH; ++y) {
@@ -118,6 +187,14 @@ public class Board {
 		return true;
 	}
 	
+	/**
+	 * Cascades down a given column,
+	 * finds the position of the first empty position starting from the bottom.
+	 * @param column the column on which the cascading function is performed.
+	 * @return the position of the lowest empty position.
+	 */
+	/* @requires column != null
+	 * @ensures \result instanceof position || \result instance of ColumnFullException */
 	private Position cascade(Column column) {
 		for (int z = 0; z < HEIGHT; ++z) {
 			if (fields[Position.indexOf(column.x, column.y, z)] == Mark.EMPTY) {
@@ -127,7 +204,18 @@ public class Board {
 		throw new ColumnFullException(column.x, column.y);
 	}
 	
-	
+	/**
+	 * Returns the amount of consecutive marks equal to the passed mark.
+	 * Starting from a position and looking in a given direction.
+	 * @param position position with a x, y and z. 
+	 * @param mark	   	mark enum, either X or O
+	 * @param direction direction with a 0 or 1 for x, y and z.
+	 * @return amount of equals marks from a position looking into a direction.
+	 */
+	/* @require position != null
+	 *  		mark != null
+	 *  		direction != null
+	 *  @ensures result >= 0 && ( \result < WIDTH || \result < DEPTH|| \result < HEIGHT)  */
 	private int consecutiveMarks(Position position, Mark mark, Direction direction) {
 		Position next = position.inDirection(direction);
 		int amount = 0;
@@ -138,4 +226,6 @@ public class Board {
 		return amount;
 	}
 	
+	
+
 }
--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -16,21 +16,52 @@ import util.container.Pair;
 import util.exception.*;
 
 
-
+/**
+ * 
+ * @author Aart Odding
+ * Client handler thread communicating between client and server.
+ * After the initial connection between the client and the server has been established,
+ * the server passes the control over to a clienthandler thread
+ * to communicate over the socket with the client. 
+ * The clienthandler then receives messages from the client over his socket input stream, 
+ * informs the server through method calls and sends information back.
+ * All information over the socket is send in the form of commands. 
+ * Either action commands, exit codes, acknowledgment
+ * s or received commands from the client or error commands.
+ * All commands from the client that are not valid are catched in the run method,
+ * the client handler informs the client with error codes according to the throws exception.
+ * 
+ */
 public class ClientHandler extends Observable implements Runnable {
 	
-	public final Socket socket;				// public so the server can close the clienthandler
+	public final Socket socket; 		// socket over which a bufferedreader/writer communicate.
+									    // public so the server can close the clienthandler
+								    	// invariant: socket != null
+	private final Server server; 	    // server that passes his connectio over
+									    // invariant: server != null
+	private final BufferedReader in;    // buffer for the incoming socket connection
+									    // invariant: in != null
+	private final BufferedWriter out; 	// buffer for the outgoing socket connection
+										// invariant: out != null
+	private final CommandParser parser; // checks wether commands oblige to the given patterns
+										// invariant: parser != null
 	
-	private final Server server;
-	private final BufferedReader in;
-	private final BufferedWriter out;
-	private final CommandParser parser;
-	
-	//private Pair<Command, String[]> lastCommandSend = null;
-	//private boolean acknowledgementPending = false;
-	//private boolean inGame = false;
-	
-	
+
+	/** 
+	 * Creates a clienthandler with a server, socket,
+	 * and bufferer reader for in and outgoing stream and a command parser.
+	 * @param serverArg
+	 * @param sockArg
+	 * @throws IOException
+	 */
+	/* @requires serverArg != null
+	 * 			 sockArg != null
+	 * @ensures  server = serverArg
+	 * 			 socket = sockArg
+	 * 			 in != null
+	 * 			 out != null
+	 * 			 parser != null
+	 * 		     parser.direction == client to server. */
 	public ClientHandler(Server serverArg, Socket sockArg) throws IOException {
 		server = serverArg;
 		socket = sockArg;
@@ -42,12 +73,23 @@ public class ClientHandler extends Observable implements Runnable {
 	
 	//////////////////////////////////////
 	//                                  //
-	//    Executing incoming commands   //
+	//  		Public Commands		    //
 	//                                  //
 	//////////////////////////////////////
 	
-	public void run() {	// TODO synchronize where necessary
-		
+	
+	/**
+	 * Executing incoming commands.
+	 * Keeps running to receive commands from the client over the socket connection.
+	 * Parses the incoming string to see wether they hold true to the protocol's 
+	 * syntactic requirements and seperates each string in a command 
+	 * and string array for the arguments.
+	 * Afterwards a distinction is made between the possible commands
+	 * (Action, Exit, Acknowledgement and Error) and handled accordingly.
+	 * All thrown errors are handled in this method and the client is notified accordingly 
+	 * with a matching error code as stated by the protocol.
+	 */
+	public void run() {	
 		while (true) {
 			try {
 				Pair<Command, String[]> command = parser.parse(in.readLine());
@@ -79,6 +121,25 @@ public class ClientHandler extends Observable implements Runnable {
 	}
 	
 	
+	//////////////////////////////////////
+	//  								//
+	// 		   Private Commands		    //
+	// 								    //
+	//////////////////////////////////////
+	
+
+	/**
+	 * Handles the received action commands from the client and throws exceptions, 
+	 * catched by the run method, accordingly.
+	 * @param action					    the action commands send from the client
+	 * @param args						    the arguments given with the command
+	 * @throws NameUnavailableException     if the client uses an unavailable name
+	 * @throws CommandForbiddenException   if a start function is received which is a server action
+	 * @throws IllegalMoveException  	   if a move is send that is not possible
+	 */
+	
+	/* @require action != null
+	 *		    args.length < 2 */
 	private void runAction(Action action, String[] args) throws 
 		NameUnavailableException, 
 		CommandForbiddenException, 
@@ -134,7 +195,6 @@ public class ClientHandler extends Observable implements Runnable {
 				server.joinChat(this);
 				sendCommand(Acknowledgement.OK);
 				break;
-				
 			case LIST:									// unsupported commands can be ignored
 				break;
 			case LEADERBOARD:
@@ -149,7 +209,16 @@ public class ClientHandler extends Observable implements Runnable {
 		
 	}
 	
-	
+	/**
+	 * Handles an acknowledgement (an answer to a send command) from the client. 
+	 * OK's are accepted, other acknowledgements should not be send from the client.
+	 * 
+	 * @param acknowledgement 			    the received acknowledgment 
+	 * @param args						    Possible arguments stating further information.
+	 * @throws CommandForbiddenException	if the command is not an acknowledgement.
+	 */
+	/* @requires acknowledgment != null
+	 *  		 args.length < 2 */
 	private void runAcknowledgement(Acknowledgement acknowledgement, String[] args) throws 
 		CommandForbiddenException {
 		switch (acknowledgement) {
@@ -173,7 +242,14 @@ public class ClientHandler extends Observable implements Runnable {
 		//sendCommand(Action.SAY, "err");
 	}
 	
-	
+	/**
+	 * Handles exit commands from the client on which the server is called to forfeit this game. 
+	 * @param exit	exit command
+	 * @param args	stating further information
+	 * @throws CommandForbiddenException thrown if the client must not send this as an exit command.
+	 */
+	/* @requires exit != null
+	 *  		 args.length < 2 */
 	private void runExit(Exit exit, String[] args) throws 
 		CommandForbiddenException {
 		if (exit == Exit.FORFEITURE) {
@@ -184,17 +260,39 @@ public class ClientHandler extends Observable implements Runnable {
 	}
 	
 	
-	///////////////////////////////////////
-	//                                   //
-	//    Sending commands over socket   //
-	//                                   //
-	///////////////////////////////////////
+	/**
+	 * Sends a command without arguments over the outgoing socket connection.
+	 * @param command
+	 */
+	/* @requires command < 2 */
+	public synchronized void sendCommand(Command command) {
+		try {
+			out.write(command.toString() + "\n");
+			out.flush();
+		} catch (IOException e) { }
+	}
 	
+	
+	/**
+	 * calls the send command with the string array parameter as one string argument.
+	 * @param command
+	 * @param arguments
+	 */
+	/* @requires command != null
+	 *  		 arguments.length > 1 */
 	public synchronized void sendCommand(Command command, String[] arguments) {
 		sendCommand(command, Util.join(arguments));
 	}
 	
 	
+	/**
+	 * Send a command to the outgoing socket if it is not empty. 
+	 * Flushes the stream and catches any Input/Output exception.
+	 * @param command
+	 * @param arguments
+	 */
+	/* @requires command != null
+	 *  		 arguments != null */
 	public synchronized void sendCommand(Command command, String arguments) {
 		String args = Util.join(Util.split(arguments));
 		try {
@@ -203,14 +301,6 @@ public class ClientHandler extends Observable implements Runnable {
 				out.write(" " + arguments);
 			}
 			out.write("\n");
-			out.flush();
-		} catch (IOException e) { }
-	}
-	
-	
-	public synchronized void sendCommand(Command command) {
-		try {
-			out.write(command.toString() + "\n");
 			out.flush();
 		} catch (IOException e) { }
 	}
